d2007bbdbe484fb21adf83ad4aeb269b538967dd
Fahim Rimon <fahimrimon21@gmail.com>
2022-04-17 00:33:32 +0600

    Route setup

---
+++ src/components/Services/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Services.css';
+const Services = () => {
+    return (
+        <div>
+            <h2>This is service</h2>
+        </div>
+    );
+};
+
+export default Services;
